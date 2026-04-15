b439e7abc2b296b5264a41fc497949f5bc812277
Tugume Andrew <tugumeandrew@gmail.com>
2026-04-15 23:02:07 +0300

    Added comment

--- Lessons/1.27/1.27.4.sql
+++ Lessons/1.27/1.27.4.sql
@@ -2,8 +2,7 @@ SELECT DISTINCT
     EXTRACT(YEAR FROM job_posted_date) AS job_posted_year,
     EXTRACT(QUARTER FROM job_posted_date) AS job_posted_quarter,
     EXTRACT(YEAR FROM job_posted_date) || '-' || EXTRACT(QUARTER FROM job_posted_date) AS job_posted_year_quarter,
-    COUNT(job_id) AS job_posts
-    
+    COUNT(job_id) AS job_posts --job_posts
 FROM
     job_postings_fact
 WHERE EXTRACT(YEAR FROM job_posted_date) BETWEEN 2023 AND 2024
